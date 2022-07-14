f5fec1b96cddbe5c6a043368f14c08ed1a15255a
Lennox Anderson <lennox55555@icloud.com>
2022-07-14 11:36:30 -0700

    API Key and Testing

--- index.js
+++ index.js
@@ -31,3 +31,24 @@ app.post('/shirts/:id', (req, res) => {
     });
 
 });
+
+
+// const https = require('https');
+//
+// var options = {
+//     "method": "GET",
+//     "hostname": "rest.coinapi.io",
+//     "path": "/v1/exchanges",
+//     "headers": {'X-CoinAPI-Key': 'C706B5ED-1032-4544-8B88-E9042708F57D'}
+// };
+//
+// var request = https.request(options, function (response) {
+//     var chunks = [];
+//
+//     response.on("data", function (chunk) {
+//         chunks.push(chunk);
+//     });
+//
+// });
+//
+// request.end();
